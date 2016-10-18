c24263521783936726c88394ca4611288499997d
Elías Manzaneda <Elas Manzaneda>
2016-10-18 13:10:54 +0200

    practica_2_10

--- app/src/main/java/masviews/example/org/masviews/MasViewsActivity.java
+++ app/src/main/java/masviews/example/org/masviews/MasViewsActivity.java
@@ -3,18 +3,36 @@ package masviews.example.org.masviews;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MasViewsActivity extends AppCompatActivity {
+    private EditText entrada;
+    private TextView salida;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mas_views);
+
+        entrada = (EditText)findViewById(R.id.entrada);
+        salida = (TextView)findViewById(R.id.salida);
     }
 
 
     public void sePulsa(View view){
-        Toast.makeText(this, "Pulsado", Toast.LENGTH_SHORT).show();
+        try{
+            String textoSalida = String.valueOf(Float.parseFloat(entrada.getText().toString())*2.0);
+            salida.setText(textoSalida);
+        }catch (Exception e){
+            Toast.makeText(getApplicationContext(), "Seleccione primero un valor.", Toast.LENGTH_LONG).show();
+        }
+
+    }
+
+    public void sePulsa0(View view){
+        String textoEntrada = entrada.getText() + (String)view.getTag();
+        entrada.setText(textoEntrada);
     }
 }
